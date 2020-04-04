5009cdc09663179d4ec98075c74f6b97e6f65f9c
Tadej Hribar <tadej.996@gmail.com>
2020-04-04 15:30:52 +0200

    Remove facebook pixel.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,32 +31,6 @@
     </script>
 @endif
 
-<script>
-    !function (f, b, e, v, n, t, s) {
-        if (f.fbq) return;
-        n = f.fbq = function () {
-            n.callMethod ?
-                n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-        };
-        if (!f._fbq) f._fbq = n;
-        n.push = n;
-        n.loaded = !0;
-        n.version = '2.0';
-        n.queue = [];
-        t = b.createElement(e);
-        t.async = !0;
-        t.src = v;
-        s = b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t, s)
-    }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '825912424423753');
-    fbq('track', 'PageView');
-</script>
-<noscript>
-    <img height="1" width="1" style="display:none"
-         src="https://www.facebook.com/tr?id=825912424423753&ev=PageView&noscript=1"/>
-</noscript>
-
 @if (env('AD_CLIENT_ID'))
     <script data-ad-client="{{ env('AD_CLIENT_ID') }}" async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
