ef5d8811d0c97281e06ed1ace5091f1955631097
Tadej Hribar <tadej.996@gmail.com>
2020-04-06 13:29:03 +0200

    Clean up question answers design.

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -23,17 +23,12 @@ class QuestionsController extends Controller
     }
 
     public function show(Question $question) {
-        $answersGroupedByDay = $question
+        $answers = $question
             ->answers()
             ->orderBy('created_at', 'DESC')
             ->with('votes')
-            ->get()
-            ->mapToGroups(function (Answer $answer) {
-                return [
-                    $answer->created_at->format('d.m.Y') => $answer,
-                ];
-            });
+            ->get();
 
-        return view('questions.show', compact('question', 'answersGroupedByDay'));
+        return view('questions.show', compact('question', 'answers'));
     }
 }
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -43,52 +43,43 @@
         </div>
     </div>
 
-    @if ($answersGroupedByDay->isNotEmpty())
+    @if ($answers->isNotEmpty())
         <div class="timeline">
-            @foreach($answersGroupedByDay as $answersInDay)
-                <div class="time-label">
-                    <span class="bg-red">
-                        {{ $answersInDay->first()->created_at->format('d.m.Y') }}
-                    </span>
-                </div>
-                @foreach($answersInDay as $answer)
-                    <div>
-                        <i class="fas fa-comment bg-blue"></i>
-                        <div class="timeline-item">
-                            <span class="time">
-                                <i class="fas fa-clock"></i>
-                                {{ $answer->created_at->format('H:i') }}
+            @foreach($answers as $answer)
+                <div>
+                    <div class="timeline-item">
+                        <span class="time">
+                            objavljeno {{ $answer->created_at->diffForHumans() }}
+                        </span>
+                        <h3 class="timeline-header">
+                            <span class="text-blue">
+                                {!! $answer->full_name ?? trans('messages.anonymous') !!}
                             </span>
-                            <h3 class="timeline-header">
-                                <span class="text-blue">
-                                    {!! $answer->full_name ?? trans('messages.anonymous') !!}
-                                </span>
-                                meni
-                            </h3>
+                            meni
+                        </h3>
 
-                            <div class="timeline-body">
-                                {{ $answer->content }}
-                            </div>
-                            <div class="timeline-footer">
-                                <x-difference-indicator :value="$answer->votes()->sum('value')" class="mr-1"/>
-                                @if ($answer->votes->where('ip_address', request()->ip())->where('value', 1)->isEmpty())
-                                    {!! Form::open(['route' => ['questions.answers.up_vote', $question, $answer], 'class' => 'd-inline-block']) !!}
-                                        <button type="submit" class="btn btn-primary btn-xs text-white">
-                                            <i class="fas fa-thumbs-up"></i>
-                                        </button>
-                                    {!! Form::close() !!}
-                                @endif
-                                @if ($answer->votes->where('ip_address', request()->ip())->where('value', -1)->isEmpty())
-                                    {!! Form::open(['route' => ['questions.answers.down_vote', $question, $answer], 'class' => 'd-inline-block']) !!}
-                                        <button type="submit" class="btn btn-danger btn-xs text-white">
-                                            <i class="fas fa-thumbs-down"></i>
-                                        </button>
-                                    {!! Form::close() !!}
-                                @endif
-                            </div>
+                        <div class="timeline-body">
+                            {{ $answer->content }}
+                        </div>
+                        <div class="timeline-footer">
+                            <x-difference-indicator :value="$answer->votes()->sum('value')" class="mr-1"/>
+                            @if ($answer->votes->where('ip_address', request()->ip())->where('value', 1)->isEmpty())
+                                {!! Form::open(['route' => ['questions.answers.up_vote', $question, $answer], 'class' => 'd-inline-block']) !!}
+                                    <button type="submit" class="btn btn-primary btn-xs text-white">
+                                        <i class="fas fa-thumbs-up"></i>
+                                    </button>
+                                {!! Form::close() !!}
+                            @endif
+                            @if ($answer->votes->where('ip_address', request()->ip())->where('value', -1)->isEmpty())
+                                {!! Form::open(['route' => ['questions.answers.down_vote', $question, $answer], 'class' => 'd-inline-block']) !!}
+                                    <button type="submit" class="btn btn-danger btn-xs text-white">
+                                        <i class="fas fa-thumbs-down"></i>
+                                    </button>
+                                {!! Form::close() !!}
+                            @endif
                         </div>
                     </div>
-                @endforeach
+                </div>
             @endforeach
         </div>
     @endif
